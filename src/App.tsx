ab4d1077d67a622ff32896b58e2060f62583d743
r6cky <cleitonrocha321@hotmail.com>
2023-06-02 10:10:18 -0300

    feat: create files structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
